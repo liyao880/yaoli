b21fbdab5f392c9132c85a7477077be064b76107
cubic_tu <cubic_tu@sina.com>
2020-07-27 19:47:27 -0400

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Los Angeles, Washington",
+    34.0200544,
+    -118.2430755
+  ],
+  [
     "Montr\u00e9al, CANADA",
     45.4972159,
     -73.6103642
@@ -10,7 +15,12 @@ var addressPoints = [
     -122.1923372
   ],
   [
-    "Chapel Hill, NC",
+    "New Orleans, Louisiana",
+    29.9499323,
+    -90.0701156
+  ],
+  [
+    "Chapel Hill, North Carolina",
     35.9131542,
     -79.05578
   ]
